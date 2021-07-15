c2fae90ea57caf991b816f904f13f32b57b5ba97
Leonardo Rodriguez <tehuelche@gmail.com>
2021-07-15 07:24:34 -0500

    Refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,62 +1,24 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
-if [[ -z "$GITHUB_REPOSITORY" ]]; then
-  echo "The env variable GITHUB_REPOSITORY is required."
-  exit 1
+PR_SIZE_LABELER_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PR_SIZE_LABELER_API="api.github.com"
+
+if [ "$PR_SIZE_LABELER_HOME" == "/" ]; then
+  PR_SIZE_LABELER_HOME=""
 fi
 
-if [[ -z "$GITHUB_EVENT_PATH" ]]; then
-  echo "The env variable GITHUB_EVENT_PATH is required."
-  exit 1
+if [ ! -z "$8" ]; then
+    PR_SIZE_LABELER_API=$8
 fi
 
-GITHUB_TOKEN="$1"
+export PR_SIZE_LABELER_HOME
+export PR_SIZE_LABELER_API
 
-URI="https://api.github.com"
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+bash --version
 
-echo "GitHub event"
-echo "$GITHUB_EVENT_PATH"
+source "$PR_SIZE_LABELER_HOME/src/main.sh"
 
-number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+main "$@"
 
-autolabel() {
-  # https://developer.github.com/v3/pulls/#get-a-single-pull-request
-  # Example: https://api.github.com/repos/CodelyTV/java-ddd-example/pulls/7
-  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}")
-
-  additions=$(echo "$body" | jq '.additions')
-  deletions=$(echo "$body" | jq '.deletions')
-  total_modifications=$(echo "$additions + $deletions" | bc)
-  label_to_add=$(label_for "$total_modifications")
-
-  echo "Labeling pull request with $label_to_add"
-
-  curl -sSL \
-    -H "${AUTH_HEADER}" \
-    -H "${API_HEADER}" \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d "{\"labels\":[\"${label_to_add}\"]}" \
-    "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels"
-}
-
-label_for() {
-  if [ "$1" -lt 10 ]; then
-    label="size/xs"
-  elif [ "$1" -lt 100 ]; then
-    label="size/s"
-  elif [ "$1" -lt 500 ]; then
-    label="size/m"
-  elif [ "$1" -lt 1000 ]; then
-    label="size/l"
-  else
-    label="size/xl"
-  fi
-
-  echo "$label"
-}
-
-autolabel
+exit $?
